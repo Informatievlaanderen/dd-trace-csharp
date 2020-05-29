ff07c6ad81394782ae81c53eb81d3bea4002e497
David Cumps <david@cumps.be>
2020-05-29 23:44:07 +0200

    test: add serilog to test web app

--- test/TestWebApp/Program.cs
+++ test/TestWebApp/Program.cs
@@ -2,6 +2,8 @@ namespace TestWebApp
 {
     using System.IO;
     using Microsoft.AspNetCore.Hosting;
+    using Serilog;
+    using Serilog.Formatting.Compact;
 
     public class Program
     {
@@ -11,6 +13,20 @@ namespace TestWebApp
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                    var loggerConfiguration = new LoggerConfiguration()
+                        .ReadFrom.Configuration(hostingContext.Configuration)
+                        .WriteTo.Console(new RenderedCompactJsonFormatter())
+                        .Enrich.FromLogContext()
+                        .Enrich.WithMachineName()
+                        .Enrich.WithThreadId()
+                        .Enrich.WithEnvironmentUserName();
+
+                    var logger = Log.Logger = loggerConfiguration.CreateLogger();
+
+                    logging.AddSerilog(logger);
+                })
                 .UseStartup<Startup>()
                 .Build()
                 .Run();
