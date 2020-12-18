43fe4048111a241d5662d4427b2145e3f02d7372
Arne Dumarey <arne.dumarey@kb.vlaanderen.be>
2020-12-18 10:57:16 +0100

    fix: move to 5.0.1

--- build.fsx
+++ build.fsx
@@ -1,8 +1,8 @@
 #r "paket:
-version 5.241.6
+version 6.0.0-beta8
 framework: netstandard20
 source https://api.nuget.org/v3/index.json
-nuget Be.Vlaanderen.Basisregisters.Build.Pipeline 4.2.3 //"
+nuget Be.Vlaanderen.Basisregisters.Build.Pipeline 5.0.1 //"
 
 #load "packages/Be.Vlaanderen.Basisregisters.Build.Pipeline/Content/build-generic.fsx"
 
@@ -14,22 +14,39 @@ open ``Build-generic``
 let assemblyVersionNumber = (sprintf "%s.0")
 let nugetVersionNumber = (sprintf "%s")
 
-let build = buildSolution assemblyVersionNumber
-let publish = publishSolution assemblyVersionNumber
+let buildSource = build assemblyVersionNumber
+let buildTest = buildTest assemblyVersionNumber
+let publishSource = publish assemblyVersionNumber
 let pack = packSolution nugetVersionNumber
 
 supportedRuntimeIdentifiers <- [ "linux-x64" ]
 
 // Library ------------------------------------------------------------------------
-Target.create "Lib_Build" (fun _ -> build "Be.Vlaanderen.Basisregisters.DataDog.Tracing")
+Target.create "Lib_Build" (fun _ ->
+  buildSource "Be.Vlaanderen.Basisregisters.DataDog.Tracing"
+  buildSource "Be.Vlaanderen.Basisregisters.DataDog.Tracing.AspNetCore"
+  buildSource "Be.Vlaanderen.Basisregisters.DataDog.Tracing.Autofac"
+  buildSource "Be.Vlaanderen.Basisregisters.DataDog.Tracing.Sql"
+  buildSource "Be.Vlaanderen.Basisregisters.DataDog.Tracing.SqlStreamStore"
+  buildTest "Be.Vlaanderen.Basisregisters.DataDog.Tracing.Tests"
+  buildTest "Be.Vlaanderen.Basisregisters.DataDog.Tracing.Sql.Tests"
+)
+
 Target.create "Lib_Test" (fun _ ->
   [
     "test" @@ "Be.Vlaanderen.Basisregisters.DataDog.Tracing.Tests"
-    "test" @@ "Be.Vlaanderen.Basisregisters.DataDog.Tracing.Sql.Tests" 
+    "test" @@ "Be.Vlaanderen.Basisregisters.DataDog.Tracing.Sql.Tests"
   ] |> List.iter testWithDotNet
 )
 
-Target.create "Lib_Publish" (fun _ -> publish "Be.Vlaanderen.Basisregisters.DataDog.Tracing")
+Target.create "Lib_Publish" (fun _ ->
+  publishSource "Be.Vlaanderen.Basisregisters.DataDog.Tracing"
+  publishSource "Be.Vlaanderen.Basisregisters.DataDog.Tracing.AspNetCore"
+  publishSource "Be.Vlaanderen.Basisregisters.DataDog.Tracing.Autofac"
+  publishSource "Be.Vlaanderen.Basisregisters.DataDog.Tracing.Sql"
+  publishSource "Be.Vlaanderen.Basisregisters.DataDog.Tracing.SqlStreamStore"
+)
+
 Target.create "Lib_Pack" (fun _ -> pack "Be.Vlaanderen.Basisregisters.DataDog.Tracing")
 
 // --------------------------------------------------------------------------------
